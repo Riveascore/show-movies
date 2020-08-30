8811a779d9e080ad22e9cd98b4b276f1131fa121
Chris Aga <chrisaga.dev@gmail.com>
2020-08-30 00:19:55 -0500

    Add MovieList

---
+++ src/stories/MovieList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import PropTypes from 'prop-types';
+import './movie_list_item.css';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export const MovieListItem = ({ title, uri, overview, ...props }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1
+        className="storybook-movie-list-item"
+      >
+        {title}
+      </h1>
+      <img
+        src={uri}
+        alt={title}
+      />
+      <p>
+        {overview}
+      </p>
+    </div>
+  );
+}
+
+MovieListItem.propTypes = {
+};
+
+MovieListItem.defaultProps = {
+};
---
+++ src/stories/MovieList.stories.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { MovieListItem } from './MovieListItem';
+
+export default {
+  title: 'Example/MovieListItem',
+  component: MovieListItem,
+};
+
+const Template = (args) => <MovieListItem {...args} />;
+
+export const SingleMovie = Template.bind({});
+SingleMovie.args = {
+  title: "Title",
+  uri: 'https://i.pinimg.com/originals/d7/87/da/d787da8630ce5dfcd2d7f375b03efc55.png',
+  overview: "Overview"
+};
