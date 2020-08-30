78afdba639c7486c938db6d30fa439cdbba0bc25
Chris Aga <chrisaga.dev@gmail.com>
2020-08-30 00:25:52 -0500

    Add data list of movies

---
+++ src/data/movies.js
@@ -0,0 +1,41 @@
+const movies = [
+  {
+    title: "Gladiator",
+    uri: "https://i.insider.com/5c82ceaceb3ce8343e2a6c2a?width=1300&format=jpeg&auto=webp",
+    overview: `
+      Ridley Scott's "Gladiator" was a critical darling when it came out in 2000, even earning the Oscar for best picture in 2001.
+
+      The action-packed drama follows former Roman General Maximus (Russell Crowe) as he fights gladiator battles in order to seek freedom from slavery and avenge the deaths of his family members.
+    `
+  },
+  {
+    title: "Sin City",
+    uri: "https://i.insider.com/5c82cd65eb3ce8343e5986e2?width=1300&format=jpeg&auto=webp",
+    overview: `
+      Frank Miller and Robert Rodriguez's "Sin City" is an action-packed thriller with Jessica Alba, Rosario Dawson, Mickey Rourke, and Bruce Willis as its leads. Adapted from Frank Miller's comic books, the movie is filled with corruption, romance, and redemption. 
+    `
+  },
+  {
+    title: "The Hangover",
+    uri: "https://i.insider.com/5c59e949eb3ce80dad237175?width=1300&format=jpeg&auto=webp",
+    overview: `
+      In Todd Phillips' "The Hangover," three friends (Zach Galifianakis, Bradley Cooper, and Ed Helms) wake up from a bachelor party in Las Vegas, Nevada with no memory of the night before and no clue where the bachelor went. Together, they wander the city and try to piece together what happened during their night. 
+    `
+  },
+  {
+    title: "Pirates of the Caribbean: The Curse of the Black Pearl",
+    uri: "https://i.insider.com/5a1319667101ad2a4e719beb?width=1300&format=jpeg&auto=webp",
+    overview: `
+      A film that resulted in an entire franchise, "Pirates of the Caribbean: The Curse of the Black Pearl" features pirates, ancient curses, and an army of the undead. Johnny Depp, Orlando Bloom, and Keira Knightley play the leads of the adventure film and they later regrouped for several sequels. 
+    `
+  },
+  {
+    title: "Requiem for a Dream",
+    uri: "https://i.insider.com/5c82d049eb3ce8305b49e952?width=1300&format=jpeg&auto=webp",
+    overview: `
+      Darren Aronofsky's "Requiem for a Dream" is a visually-driven drama about drug addiction that stars Jared Leto, Jennifer Connelly, Marlon Wayans, and Ellen Burstyn. The film follows the four main characters as they grapple with different addictions that alter their interpretations of reality. 
+    `
+  }
+];
+
+module.exports = movies
